fbbe7e4cebc8ab16fb10e441fe7737a828bcb170
anuraggoswami <anuraggoswami0@gmail.com>
2025-11-14 21:08:04 +0530

    bg change

--- app/src/main/java/com/anurag/portfoliotask/MyApp.kt
+++ app/src/main/java/com/anurag/portfoliotask/MyApp.kt
@@ -1,10 +1,12 @@
 package com.anurag.portfoliotask
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.anurag.portfoliotask.navigation.MainNavGraph
 
 @Composable
@@ -12,7 +14,7 @@ fun MyApp() {
     MaterialTheme {
         Surface(
             modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background
+            color = Color.White
         ) {
             MainNavGraph()
         }
